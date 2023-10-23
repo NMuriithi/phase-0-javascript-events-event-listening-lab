9ad58d054d24a5eb3b38b119e5d1710ede4ec7f7
Nicholus <nicholus.muriithi@student.moringaschool.com>
2023-10-23 09:09:52 -0700

    addEventListener() lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const inputs = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+inputs.addEventListener('click', clickAlert);
+
 function addingEventListener() {
+    let inputs = document.getElementById('input');
+    for (let input of inputs){
+        input.addEventListener('click',addingEventListener);
+    }
 }
